b7f0011813fc19654341830a02b3bdc6e0c367ef
DyogoBendo <dyogoromagnabendo@gmail.com>
2021-02-03 17:02:01 -0300

    Adicionando namespace

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-
 from . import views
 
+app_name = "polls"  # definindo o namespace da aplicação e de suas rotas
 urlpatterns = [
     path('', views.index, name='index'),  # mapeamos a url do index
     # adicionamos o caminho para acessar a view detail, passando o parametro question_id, que é um inteiro
