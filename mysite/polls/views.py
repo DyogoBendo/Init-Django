978baa778a9b4a6ae9b96b3caabd9b19067ce895
DyogoBendo <dyogoromagnabendo@gmail.com>
2021-02-03 14:51:22 -0300

    Pegando informações dos models na view

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
+import json
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("Hello world!")  # resposta gerada quando essa função for chamada
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]  # pegamos os cinco ultimas questions publicadas
+    output = ' '.join([q.question_text for q in latest_question_list])  # pega cada um dos titulos das questions
+    return HttpResponse(output)
 
 
 def detail(request, question_id):
