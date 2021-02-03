1665e049d8265ae7ce8b81f7bed1445b26fcd7a9
DyogoBendo <dyogoromagnabendo@gmail.com>
2021-02-03 14:32:15 -0300

    Criando novas Views

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -3,5 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')  # mapeamos a url do index
+    path('', views.index, name='index'),  # mapeamos a url do index
+    # adicionamos o caminho para acessar a view detail, passando o parametro question_id, que é um inteiro
+    path('<int:question_id>/', views.detail, name="detail"),
+    # caminho para results
+    path('<int:question_id>/results/', views.results, name='results'),
+    # caminho para votar
+    path("<int:question_id>/vote/", views.vote, name="vote")
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -7,3 +7,15 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("Hello world!")  # resposta gerada quando essa função for chamada
 
+
+def detail(request, question_id):
+    return HttpResponse(f"Você está olhando a questão {question_id}")
+
+
+def results(request, question_id):
+    response = f'Você está olhando para os resultados da questão {question_id}'
+    return HttpResponse(response)
+
+
+def vote(request, question_id):
+    return HttpResponse("Você está votando na questão %s" % question_id)
