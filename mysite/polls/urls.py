9b9b13712f703440a411b341428ffc9d6390c638
DyogoBendo <dyogoromagnabendo@gmail.com>
2021-02-04 18:39:59 -0300

    Corrigindo url para views genéricas

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -3,11 +3,11 @@ from . import views
 
 app_name = "polls"  # definindo o namespace da aplicação e de suas rotas
 urlpatterns = [
-    path('', views.index, name='index'),  # mapeamos a url do index
+    path('', views.IndexView.as_view(), name='index'),  # mapeamos a url do index
     # adicionamos o caminho para acessar a view detail, passando o parametro question_id, que é um inteiro
-    path('<int:pk>/', views.detail, name="detail"),
+    path('<int:pk>/', views.DetailView.as_view(), name="detail"),
     # caminho para results
-    path('<int:pk>/results/', views.results, name='results'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     # caminho para votar
     path("<int:question_id>/vote/", views.vote, name="vote")
 ]
