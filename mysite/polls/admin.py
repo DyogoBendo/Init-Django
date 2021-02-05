59891a48c48966c63e10c55b1f9fb0989d4b3533
DyogoBendo <dyogoromagnabendo@gmail.com>
2021-02-05 15:21:14 -0300

    Adicionando choices dentro das Questions

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,14 +1,20 @@
 from django.contrib import admin
-from .models import Question
+from .models import Question, Choice
 
 # Register your models here.
+
+
+class ChoiceInLine(admin.StackedInline):
+    model = Choice
+    extra = 3  # são mostradas pelo menos 3 campos de Choice
 
 
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None, {'fields': ['question_text']}),
-        ('Date Information', {"fields" : ['pub_date']}),
+        ('Date Information', {"fields" : ['pub_date'], "classes": ["collapse"]}),
     ]
+    inlines = [ChoiceInLine]  # adicionamos para que Choice possa ser editado em Question
 
 
 admin.site.register(Question, QuestionAdmin)
