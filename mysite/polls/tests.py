ab2432042424157261659ad52c6535a10d4d26c2
DyogoBendo <dyogoromagnabendo@gmail.com>
2021-02-04 21:31:45 -0300

    Concluindo testes do programa

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -92,3 +92,23 @@ class QuestionIndexViewsTests(TestCase):
             response.context["latest_question_list"],
             ["<Question: Past question 2>", "Question: Past question 1>"]
         )
+
+
+class QuestionDetailsViewTests(TestCase):
+    def test_future_question(self):
+        """
+        Para uma visualização de uma questão no futuro, o site deve retornar 404
+        """
+        future_question = create_questions(question_text="Future question", days=5)
+        url = reverse("polls:detail", args=(future_question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+    def test_past_question(self):
+        """
+        Para uma questão no passado, a view detail mostra o texto
+        """
+        past_question = create_questions(question_text="Past question", days=-5)
+        url = reverse("polls:detail", args=(past_question.id,))
+        response = self.client.get(url)
+        self.assertContains(response, past_question.question_text)
